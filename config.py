09f86536f51082fd1e23be2ce3384b6de22fa85b
Nguyen Thanh Tuan <nguyentuan02156@gmail.com>
2025-11-26 21:02:39 +0700

    refactor: remove all capsnet-related code, chỉ giữ custom_xception

--- config.py
+++ config.py
@@ -4,15 +4,13 @@ CLASS_NAMES = ['Bacterial Leaf Blight', 'Brown Spot', 'Healthy Rice Leaf', 'Leaf
 INPUT_SHAPE = (256, 256, 3)
 NUM_CLASSES = len(CLASS_NAMES)
 
-# Đường dẫn model (có thể thay đổi cho từng model)
-KERAS_MODEL_PATH = "models/basic_capsnet_best.keras"
+
 WEIGHTS_PATH = "models/model_fold_5.weights.h5"
 
-# Định nghĩa pipeline preprocessing cho từng model
+
 PREPROCESSING_PIPELINES = {
-    "capsnet": ["wiener", "otsu"],
     "custom_xception": ["none"]
 }
 
 # Model đang sử dụng
-ACTIVE_MODEL = "capsnet"
+ACTIVE_MODEL = "custom_xception"
